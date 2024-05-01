c56af44bd582db1dfde0c218239d389a55453976
Mahdi <mahdi.sundarani@gmail.com>
2024-05-01 15:54:43 +0530

    crud with axios

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import TableContainer from './components/TableContainer';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='underline underline-offset-8 pb-7 font-bold'>API CRUD WITH AXIOS</h1>
+    <div className="App bg-slate-200 h-screen">
+     <h1 className='underline underline-offset-8 pt-4 font-bold'>API CRUD WITH AXIOS</h1>
      <TableContainer/>
     </div>
   );
